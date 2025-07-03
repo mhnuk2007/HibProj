93235965ef131172ac44cb8e29001c01d6927e80
Mohan Lal <mhnuk2007@gmail.com>
2025-07-03 18:41:40 +0500

    Added code to delete student by ID using session.remove

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -5,19 +5,9 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first hibernate project");
-
-        Student s1 = new Student();
-        s1.setRollNo(102);
-        s1.setsName("Munesh");
-        s1.setsAge(20);
-
-        Student s2 = null;
-
-
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(com.learning.Student.class)
@@ -27,11 +17,17 @@ public class Main {
         Session session = sf.openSession();
 
         Transaction transaction = session.beginTransaction();
-        session.merge(s1);
+
+        Student s = session.get(Student.class, 102); // load from DB
+        if (s != null) {
+            session.remove(s);  // delete from DB
+            System.out.println("Deleted: " + s);
+        } else {
+            System.out.println("Student not found.");
+        }
+
         transaction.commit();
         session.close();
         sf.close();
-        System.out.println(s1);
-
     }
 }
