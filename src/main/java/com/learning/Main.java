d2c6604e7d79773aa4aa13a2f37e64bd9bb3c342
Mohan Lal <mhnuk2007@gmail.com>
2025-07-03 15:41:50 +0500

    Updated Hibernate config and entity

--- src/main/java/com/learning/Main.java
+++ src/main/java/com/learning/Main.java
@@ -1,17 +1,38 @@
 package com.learning;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("My first hibernate project");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Student s1 = new Student();
+        s1.setRollNo(107);
+        s1.setsName("Sandeep");
+        s1.setsAge(21);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(com.learning.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(s1);
+
+        transaction.commit();
+
+        session.close();
+
+
+        System.out.println(s1);
+
+
     }
 }
--- src/main/java/com/learning/Student.java
+++ src/main/java/com/learning/Student.java
@@ -1,4 +1,48 @@
 package com.learning;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+    @Id
+    private int rollNo;
+    private String sName;
+    private int sAge;
+
+    // Getters and setters
+
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public int getsAge() {
+        return sAge;
+    }
+
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", sName='" + sName + '\'' +
+                ", sAge=" + sAge +
+                '}';
+    }
 }
